ecbd3b10916dab794c3943db5cef77ca2c67b19d
19Sonali <109802850+19Sonali@users.noreply.github.com>
2024-10-09 23:48:21 +0530

    Added home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import Navbar from "./components/Navbar/Navbar"
+import HomePage from "./routes/homePage/HomePage"
 import "./layout.scss"
 
 function App() {
   return (
     <div className="layout">
+      <div className="navbar">
       <Navbar/>
+      </div>
+      <div className="content">
+      <HomePage/>
+      </div>
     </div>
   )
 }
---
+++ src/routes/homePage/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './HomePage.scss'
+
+const HomePage = () => {
+  return (
+    <div className="homePage">
+        <div className="textContainer">text</div>
+        <div className="imgContainer">img</div>
+    </div>
+  )
+}
+
+export default HomePage
