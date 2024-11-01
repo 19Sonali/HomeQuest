5975efcc9277feac47b60ac493f3be9aefe5f487
19Sonali <109802850+19Sonali@users.noreply.github.com>
2024-11-01 23:54:48 +0530

    Updated profile page

--- src/routes/ProfilePage/ProfilePage.jsx
+++ src/routes/ProfilePage/ProfilePage.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
-import './ProfilePage.scss'
+import React from "react";
+import "./ProfilePage.scss";
 
 const ProfilePage = () => {
   return (
-    <div className='profilePage'>ProfilePage</div>
-  )
-}
+    <div className="profilePage">
+      <div className="details">
+        <div className="wrapper">
+          <div className="title">
+            <h1>User Information</h1>
+            <button>Update Profile</button>
+          </div>
+          <div className="info">
+            <span>
+              Avatar:
+              <img
+                src="https://e7.pngegg.com/pngimages/178/595/png-clipart-user-profile-computer-icons-login-user-avatars-monochrome-black.png"
+                alt=""
+              />
+            </span>
+            <span>Username: <b>John Doe</b></span>
+            <span>E-mail: <b>John@gmail.com</b></span>
+          </div>
+          <div className="title">
+            <h1>My List</h1>
+            <button>Create New Posts</button>
+          </div>
+          <div className="title">
+            <h1>Saved List</h1>
+          </div>
+        </div>
+      </div>
+      <div className="chatContainer">
+        <div className="wrapper"></div>
+      </div>
+    </div>
+  );
+};
 
-export default ProfilePage
+export default ProfilePage;
